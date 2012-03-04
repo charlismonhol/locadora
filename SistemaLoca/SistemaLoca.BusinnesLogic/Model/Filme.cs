8a758af9657b4aacb7896d6eb3aec60bdee69eff
Charlis Renato Monhol <charlis.monhol@el.com.br>
2012-03-03 22:37:06 -0300

    projeto businnesLogic

---
+++ SistemaLoca/SistemaLoca.BusinnesLogic/Model/Filme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SistemaLoca.BusinnesLogic.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaLoca.BusinnesLogic.Model
+{
+    public class Filme
+    {
+        //O EF trata a propriedade ID ou Id como PK por convenção
+        //[key]
+        //[Column("codigo")]
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+
+        // associação de muitos para um 
+        // virtual é necessário para habilitar o lazy loading
+        public virtual List<ItemFilme> Itens { get; set; }
+
+    }
+}
---
+++ SistemaLoca/SistemaLoca.BusinnesLogic/Model/SistemaLocaDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using SistemaLoca.BusinnesLogic.Model;
+
+namespace SistemaLoca.BusinnesLogic.Model
+{
+    public class SistemaLocaDBContext : DbContext
+    {
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<ItemFilme> ItensFilme { get; set; }
+
+    }
+}
