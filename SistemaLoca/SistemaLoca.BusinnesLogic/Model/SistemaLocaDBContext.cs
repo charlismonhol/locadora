2bd7b63bbd0503d0d2d7a1e5ab39898b2833c9c2
Charlis Renato Monhol <charlis.monhol@el.com.br>
2012-03-25 23:21:13 -0300

    alteração do projeto

--- SistemaLoca/SistemaLoca.BusinnesLogic/Model/SistemaLocaDBContext.cs
+++ SistemaLoca/SistemaLoca.BusinnesLogic/Model/SistemaLocaDBContext.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.Entity;
-using SistemaLoca.BusinnesLogic.Model;
+using SistemaLoca.BusinnesLogic.Model.ControleAcervo;
+
 
 namespace SistemaLoca.BusinnesLogic.Model
 {
     public class SistemaLocaDBContext : DbContext
     {
+        //Controle de Acervo
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<ItemFilme> ItensFilme { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Midia> Midias { get; set; }
+        public DbSet<Distribuidora> Distribuidoras { get; set; }
+
+        //Atendimento ao Cliente
 
     }
 }
